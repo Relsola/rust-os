5ace087395ee0011887530fa4dea8b05f11dfb44
Relsola <sola001018@gmail.com>
2025-04-17 23:39:32 +0800

    编译内核

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-#![no_std]
-#![no_main]
+#![no_std] // 不链接 Rust 标准库
+#![no_main] // 禁用所有 Rust 层级的入口点
 
-#[unsafe(no_mangle)]
+#[unsafe(no_mangle)] // 不重整函数名
 pub extern "C" fn _start() -> ! {
     loop {}
 }
 
-#[panic_handler]
+#[panic_handler] // 这个函数将在 panic 时被调用
 fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
 }
